7eb18003d9025c80450952a4bf6272fc5a4f9d06
hegunhee <gunhee0072@gmail.com>
2024-08-19 22:15:28 +0900

    [simple-memo] build : 가계부 프로젝트 설정

---
+++ simplememo/simplememo/src/main/java/gunhee/simplememo/SimpleMemoApplication.java
@@ -0,0 +1,13 @@
+package gunhee.simplememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMemoApplication.class, args);
+	}
+
+}
---
+++ simplememo/simplememo/src/test/java/gunhee/simplememo/SimplememoApplicationTests.java
@@ -0,0 +1,13 @@
+package gunhee.simplememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplememoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
