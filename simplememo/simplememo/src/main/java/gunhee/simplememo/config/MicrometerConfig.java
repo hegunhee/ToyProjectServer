95ca33f6c177a5f2e6895899dcd08b2f3f0e078a
hegunhee <gunhee0072@gmail.com>
2024-09-26 11:56:16 +0900

    [simple-memo-monitiring] feat : Counter Aspect 추가

--- simplememo/simplememo/src/main/java/gunhee/simplememo/config/MicrometerConfig.java
+++ simplememo/simplememo/src/main/java/gunhee/simplememo/config/MicrometerConfig.java
@@ -1,2 +1,15 @@
-package gunhee.simplememo.config;public class MicrometerConfig {
+package gunhee.simplememo.config;
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicrometerConfig {
+
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry registry) {
+        return new CountedAspect((registry));
+    }
 }
